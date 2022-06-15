02d21f12918000cc0da04d73292fc51c11c96d88
robertsalonderis <98739453+robertsalonderis@users.noreply.github.com>
2022-06-16 01:48:10 +0300

    Koda iesākšana

--- Tests par sazarojuma kontrukcijām C# valodā Unity dzinī/Assets/Skripti/Menedzeris.cs
+++ Tests par sazarojuma kontrukcijām C# valodā Unity dzinī/Assets/Skripti/Menedzeris.cs
@@ -5,4 +5,18 @@ using UnityEngine;
 public class Menedzeris : MonoBehaviour
 {
 
+	public GameObject[] Limeni;
+	public GameObject EkranaAties, Beigas;
+
+	int currentLimenis;
+
+	public void nepareizaAtbilde()
+	{
+		EkranaAties.SetActive(true);
+	}
+
+	public void SpelesAties()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
 }
